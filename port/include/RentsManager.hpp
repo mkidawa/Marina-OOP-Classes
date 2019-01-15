9f1932dc97e21490542008f57131cb60344e5c07
awnuk <aleksandrawnuk0@gmail.com>
2019-01-15 20:30:06 +0100

    RentsManager

--- port/include/RentsManager.hpp
+++ port/include/RentsManager.hpp
@@ -5,9 +5,19 @@
 #ifndef POBIMARINA_RENTSMANAGER_HPP
 #define POBIMARINA_RENTSMANAGER_HPP
 
+#include <string>
+#include <vector>
+#include "Repo.hpp"
+#include "Rent.hpp"
 
 class RentsManager {
+    Repo<Rent_ptr> currentRents;
+    Repo<Rent_ptr> archiveRents;
 
+public:
+    Rent_ptr rent(PosixTime start, PosixTime stop, Place_ptr where, Renter_ptr who);
+    void endRent(Place_ptr place);
+    //Place_ptr findPlace(Rent_ptr rent);
 };
 
 
